6f94902822269b11e5806fe13c40d3d2492bbd15
Corey <corey.rodgers@rocketlab.com.au>
2020-08-05 01:29:46 +1000

    save user googleId in mongoDB

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const keys = require("./config/keys");
 
 mongoose.connect(keys.mongoURI);
+require('./models/User');
 require('./services/passport');
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require("passport");
 const keys = require("../config/keys");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(
     new GoogleStrategy(
@@ -10,8 +13,9 @@ passport.use(
         callbackURL: "/auth/google/callback",
       },
       (accessToken, refreshToken, profile, done) => {
-  
-        console.log('profile', profile);
+  new User({
+    googleId: profile.id
+  }).save();
       }
     )
   );
