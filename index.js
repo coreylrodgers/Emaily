826ad37142b9c8195357f7e66c07cf637cad6714
Corey <corey.rodgers@rocketlab.com.au>
2020-08-04 21:33:41 +1000

    Change name greeting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
+  res.send({ name: "Corey" });
 });
 
 const PORT = process.env.PORT || 5000
