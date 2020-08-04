ffb950fb9f2a712852dc508ec895a8cccc34f8d8
Corey <corey.rodgers@rocketlab.com.au>
2020-08-05 01:14:19 +1000

    passport and mongoose setup

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const keys = require("./config/keys");
+
+mongoose.connect(keys.mongoURI);
+require('./services/passport');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send({ name: "Corey" });
-});
+require('./routes/authRoutes')(app);
 
-const PORT = process.env.PORT || 5000
+app.get('/', (req,res) => res.send('working'));
 app.listen(PORT, () => console.log(`server running on port:${PORT}`));
+
+
